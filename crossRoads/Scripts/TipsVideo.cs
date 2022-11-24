d2dbca2b9baceaa81d83a4ba6495f01e29060a52
anderson <anderson_r_santos17@hotmail.com>
2022-11-24 00:30:44 -0300

    criado sistema de dicas com video e o sistema de pause

---
+++ crossRoads/Scripts/TipsVideo.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class TipsVideo : Area
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    private Control uiTipsVideo;
+    private VideoPlayer videoTips;
+    private mainScene scMainScene;
+
+    public override void _Ready()
+    {
+        uiTipsVideo = GetNode<Control>("Control");
+        videoTips = GetNode<VideoPlayer>("Control/VideoPlayer");
+        scMainScene = GetTree().Root.GetNode<mainScene>("rootTree");
+    }
+
+    private void showTipsVideo(Node body)
+    {
+        scMainScene.pauseGame();
+        uiTipsVideo.Visible = true;
+        videoTips.Play();
+    }
+    private void deleteTipsVideo()
+    {
+        scMainScene.pauseGameEnd();
+        QueueFree();
+    }
+}
---
+++ crossRoads/Scripts/mainScene.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class mainScene : Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+    public void pauseGame()
+    {
+        GetTree().Paused = true;
+    }
+    public void pauseGameEnd()
+    {
+        GetTree().Paused = false;
+    }
+}
