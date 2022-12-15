a7317656b2b5577daaadf3fa334110b1c7f8197c
anderson <anderson_r_santos17@hotmail.com>
2022-12-15 00:51:49 -0300

    adc o veiculo da intro e elementos do cenario como montanhas,rochas..e os tentaculos da estrada estao mirando no player

--- crossRoads/Scripts/road_tentacles.cs
+++ crossRoads/Scripts/road_tentacles.cs
@@ -1,46 +1,99 @@
 using Godot;
 using System;
-using System.Collections.Generic;
+
 
 public class road_tentacles : Spatial
 {
-    private AnimationPlayer [] animTenTacles = new AnimationPlayer[4];
+    private AnimationPlayer [] animTenTacles;
     private AnimationNodeStateMachinePlayback stateMachine;
     private AnimationTree animTree;
+    private KinematicBody player;
 
-   
+
+    private enum stateTentacles{
+      WAIT,AIM,ATTACK
+    }
+    private stateTentacles currentState;
 
     public override void _Ready()
     {
+        player = GetTree().Root.GetNode<KinematicBody>("rootTree/Player");
         Spatial allTentacles = GetNode<Spatial>("tentaclesChild");
+        animTenTacles = new AnimationPlayer[allTentacles.GetChildCount()];
         for(int i = 0 ; i < allTentacles.GetChildCount() ; i++)
         {
+
           Spatial currentTentacle = (Spatial)allTentacles.GetChild(i);
-          animTenTacles[i] = currentTentacle.GetNode<AnimationPlayer>("./AnimationPlayer");
+          animTenTacles[i] = currentTentacle.GetNode<AnimationPlayer>("AnimationPlayer");
+          animTenTacles[i].Play("swing");
           GD.Print("name Tentacles " + currentTentacle.Name);
 
         }
+
+        currentState = stateTentacles.AIM;
     }
 
-    private  async void playAsyncAnimation ()
+    private  async void wait ()
     {
         
-        
+ 
       foreach(AnimationPlayer animPlayerTentacles in animTenTacles)
       {
         await ToSignal(GetTree().CreateTimer(0.5f),"timeout");
         if(!animPlayerTentacles.IsPlaying()){
-            animPlayerTentacles.Play("swing001",-1,(float)GD.RandRange(1.0,2.0));
+            animPlayerTentacles.Play("swing",-1,(float)GD.RandRange(1.0,2.0));
         }
       }
+    }
+
+
+    
+    private void attack(AnimationPlayer animPlayer)
+    {
+      animPlayer.Play("attack");
+    }
+
+
+
+    private void aim()
+    {
+      float shorterDistance = 1000;
+      //Spatial shorterTentacle = null;
+
+      foreach(AnimationPlayer animPlayerTentacles in animTenTacles)
+      {
+        Spatial tentacleNode = (Spatial)animPlayerTentacles.GetParent();
+        float currentDistance = tentacleNode.Transform.origin.DistanceTo(player.Transform.origin);
+
+        if(currentDistance <= shorterDistance)
+        {
+          shorterDistance = currentDistance;
+          animPlayerTentacles.Play("aiming");
+          tentacleNode.LookAt(player.Transform.origin,Vector3.Up);
+          tentacleNode.RotateObjectLocal(Vector3.Up,3.14f);
+          //attack(animPlayerTentacles);
+        }
+     }
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
  public override void _Process(float delta)
  {
-    //animTree.
-  // animTenTacles.Play();
-    playAsyncAnimation();
+       switch(currentState)
+      {
+        case stateTentacles.WAIT:
+            wait();
+        break; 
+
+        case stateTentacles.AIM:
+          aim();
+        break;
+        case stateTentacles.ATTACK:
+          //attack();
+        break;
+
+
+      }
 
  }
 }
